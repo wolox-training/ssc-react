7efa30521b21d250aa68c8d6c7e5f073f493a72c
Sara Santacruz <sara.santacruz@wolox.co>
2019-07-30 11:26:39 -0500

    delay works well

--- react/finger-exercises/ecmascript/02 - promises/index.js
+++ react/finger-exercises/ecmascript/02 - promises/index.js
@@ -1,7 +1,14 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+  return new Promise((resolve, reject) => {
+    if (time < 600) {
+      setTimeout(() => resolve(time), time);
+    } else {
+      const reason = new Error('This time is too much !');
+      reject(reason);
+    }
+  });
 }
 
 export function asyncDelay() {

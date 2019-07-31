fbb16726758c2e35c48a89354ef8eaa33581eb28
Sara Santacruz <sara.santacruz@wolox.co>
2019-07-31 10:00:41 -0500

    filterAtt changed

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -14,7 +14,6 @@ export function reverseMerge(val, val2) {
   return [...val2, ...val];
 }
 
-export function filterAttribs(val) {
-  const { a, b, ...rest } = val;
+export function filterAttribs({ a, b, ...rest }) {
   return rest;
 }

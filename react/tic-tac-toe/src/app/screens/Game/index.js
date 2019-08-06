4d96701fb3f448729f6b5a7e7b0044930ae1c5fb
Sara Santacruz <sara.santacruz@wolox.co>
2019-08-06 13:55:23 -0500

    list of gamers and spinner added

--- react/tic-tac-toe/src/app/screens/Game/index.js
+++ react/tic-tac-toe/src/app/screens/Game/index.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import getMatches from '../../../services/MatchesService';
 
 import styles from './styles.module.scss';
 import Board from './components/Board';
 
-function Game () {
-  return (
-    <div className={styles.game}>
-      <div className={styles.gameBoard}>
-        <Board />
-      </div>
-      <div className={styles.gameInfo}>
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
+const Spinner = require('react-spinkit');
+
+class Game extends Component {
+  state = {
+    matches: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    getMatches.getMatches().then(response => {
+      this.setState({ matches: response.data, loading: false });
+    });
+  }
+
+  renderList = item => (
+    <li key={item.id} className={styles.itemInfo}>
+      <p>{item.player_one} - </p>
+      <p>{item.player_two} - </p>
+      <p>{item.winner}</p>
+    </li>
   );
+
+  render() {
+    return (
+      <div className={styles.game}>
+        <div className={styles.gameBoard}>
+          <Board />
+        </div>
+        <div className={styles.gameInfo}>
+          {
+            this.state.loading
+              ? <Spinner name="circle" fadeIn="none" />
+              : <ol className={styles.infoContainer}>{this.state.matches.map(this.renderList)}</ol>
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Game;
